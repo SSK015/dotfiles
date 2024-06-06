85b9106d60898f6c34ba16c44974438457245246
ssk015 <18252677381@163.com>
2024-06-06 12:47:55 +0000

    Update zshrc

--- deployzsh.sh
+++ deployzsh.sh
@@ -1,9 +1,11 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+cp .zshrc ~/.zshrc
+
 cd ~/.oh-my-zsh/plugins
 
 git clone https://github.com/zsh-users/zsh-completions 
 git clone https://github.com/zsh-users/zsh-history-substring-search 
 git clone https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/web-search  
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git  
-git clone https://github.com/catppuccin/zsh-syntax-highlighting.git ~/.zsh 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
